068acfda716daa42b5d4336240698fe9c41fb74a
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-03-12 00:39:55 -0800

    Fixed some files

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,9 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
 	first_char = None
+        new_tuple = (0, first_char)
+        return new_tuple
     else:
 	first_char = sentence[0]
-    return len(sentence), first_char
+        new_tuple = (len(sentence), first_char)
+        return new_tuple
