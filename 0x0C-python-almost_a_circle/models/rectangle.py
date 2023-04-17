bb6938e4c97b90911b4c4cea0fc605759605822c
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-04-17 15:12:45 -0700

    Fixed files for almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,48 +1,65 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Defines a rectangle class."""
 from models.base import Base
 
+
 class Rectangle(Base):
-    """Defines a Rectangle class that inherits from Base."""
+    """Represent a rectangle."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Instantiate the Rectangle class and call super to inherit from the Base class."""
-        super().__init__(id)
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+
     @property
     def width(self):
-        """Getter for twe width value. Returns the private width value."""
+        """Set/get the width of the Rectangle."""
         return self.__width
+
     @width.setter
     def width(self, value):
-        """Setter for the width value."""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
+
     @property
     def height(self):
-        """Getter for twe height value. Returns the private height value."""
+        """Set/get the height of the Rectangle."""
         return self.__height
+
     @height.setter
     def height(self, value):
-        """Setter for the height value."""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
     @property
     def x(self):
-        """Get the x coordinate of the Rectangle."""
+        """Set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -50,28 +67,32 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Get the y coordinate of the Rectangle."""
+        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-    def area (self):
-        """Returns the area of the Rectangle."""
+
+    def area(self):
+        """Return the area of the Rectangle."""
         return self.width * self.height
+
     def display(self):
         """Print the Rectangle using the `#` character."""
         if self.width == 0 or self.height == 0:
             print("")
             return
-        for _ in range(self.y):
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
             print("")
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
 
     def update(self, *args, **kwargs):
         """Update the Rectangle.
@@ -130,4 +151,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
