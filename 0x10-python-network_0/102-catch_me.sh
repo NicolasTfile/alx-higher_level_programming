7364f3682f1aecfe46446ac7b5bc32ea6da0a678
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-07-01 16:37:02 +0100

    Fixed files for python network 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that get the message You got me!
-curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
