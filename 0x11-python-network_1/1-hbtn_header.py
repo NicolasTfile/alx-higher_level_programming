eb95e1dff19b5544ef0d15805926a1727de7ebfd
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-07-02 15:58:15 +0100

    Fixed files for python network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Displays the value of the X-Request-Id variable found in the header of the response.
-Usage: ./1-hbtn_header.py <URL>
+"""
+Python script that takes in a URL, sends a request to the URL
 """
 import urllib.request
 import sys
@@ -8,6 +8,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        req_id = response.headers['X-Request-Id']
-        print(req_id)
+    with urllib.request.urlopen(url) as content:
+        request_id = content.headers['X-Request-Id']
+        print(request_id)
