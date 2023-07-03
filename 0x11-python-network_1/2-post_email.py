f411ec24de171b4a772b56e4128904a4605deb45
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-07-03 07:45:46 +0100

    Fixed files for python network 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter. Displays the body of the response (decoded in utf-8).
+"""Takes in a URL and an email, sends a POST request to the passed URL.
 Usage: ./2-post_email.py <URL> <email>
 """
-import urllib.request
-import urllib.parse
-import sys
+from urllib.request import urlopen
+from urllib.parse import urlencode
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    with urllib.request.urlopen(url, data=data) as response:
+    url = argv[1]
+    email = argv[2]
+    data = urlencode({'email': email}).encode('utf-8')
+    with urlopen(url, data=data) as response:
         response_body = response.read().decode('utf-8')
         print(response_body)
